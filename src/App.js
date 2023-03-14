c1a106481097eaa0050a27cb2cc15e498787a586
sodiq bakare <olawale0099@gmail.com>
2023-03-14 19:13:33 +0000

    first comit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Header from "./Header";
+import All from "./All";
+
+function App() {
+  return (
+    <div className="app">
+      <Header />
+      <All />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Header.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="phase1">
+        <img className="image" src="./logo192.png" />
+        <p className="img2">ReactFacts</p>
+      </div>
+
+      <div>
+        <p>React Course -Project 1</p>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
